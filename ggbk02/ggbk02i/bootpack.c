f83786c2a1abd6c35ef4404e3ff3168fc29660c0
Ryoh Akiyoshi <ryo614@gmail.com>
2021-04-24 23:24:26 +0900

    5-9
    GDT と IDT を初期化した

--- ggbk02/ggbk02i/bootpack.c
+++ ggbk02/ggbk02i/bootpack.c
@@ -22,6 +22,9 @@ extern void io_cli(void);
 extern void io_out8(int port, int data);
 extern int io_load_eflags(void);
 extern void io_store_eflags(int eflags);
+extern void load_gdtr(int limit, int addr);
+extern void load_idtr(int limit, int addr);
+
 extern void mysprintf(char *str, char *fmt, ...);
 
 void init_palette(void);
@@ -33,6 +36,22 @@ void putfonts8_asc(unsigned char *vram, int xsize, int x, int y, char c, char *s
 void init_mouse_cursor8(char *mouse, char bc);
 void putblock8_8(unsigned char *vram, int vxsize, int pxsize,
     int pysize, int px0, int py0, char *buf, int bxsize);
+
+struct SEGMENT_DESCRIPTOR {
+    short limit_low, base_low;
+    char base_mid, access_right;
+    char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+    short offset_low, selector;
+    char dw_count, access_right;
+    char offset_high;
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 
 struct BOOTINFO {
     char cyls, leds, vmode, reserve;
@@ -214,5 +233,53 @@ void putblock8_8(unsigned char *vram, int vxsize, int pxsize,
             vram[(py0+y) * vxsize + (px0+x)] = buf[y * bxsize + x];
         }
     }
+    return;
+}
+
+void init_gdtidt(void)
+{
+    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+    struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
+    int i;
+
+    // Init GDT
+    for (i = 0; i < 8192; i++) {
+        set_segmdesc(gdt + i, 0, 0, 0);
+    }
+    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+    set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a); // for bootpack.hrb
+    load_gdtr(0xffff, 0x00270000);
+
+    // Init IDT
+    for (i = 0; i < 256; i++) {
+        set_gatedesc(idt + i, 0, 0, 0);
+    }
+    load_idtr(0x7ff, 0x0026f800);
+
+    return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+    if (limit > 0xfffff) {
+        ar |= 0x8000; // G_bit = 1
+        limit /= 0x1000;
+    }
+    sd->limit_low    = limit & 0xffff;
+    sd->base_low     = base & 0xffff;
+    sd->base_mid     = (base >> 16) & 0xff;
+    sd->access_right = ar & 0xff;
+    sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high    = (base >> 24) & 0xff;
+    return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+    gd->offset_low   = offset & 0xffff;
+    gd->selector     = selector;
+    gd->dw_count     = (ar >> 8) & 0xff;
+    gd->access_right = ar & 0xff;
+    gd->offset_high  = (offset >> 16) & 0xffff;
     return;
 }
